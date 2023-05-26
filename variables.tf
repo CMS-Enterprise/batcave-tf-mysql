501671a7db044d70c6dfc42c4147ade80c900f9c
Spencer Ho <83427905+revaspeho@users.noreply.github.com>
2023-05-26 12:21:54 -1000

    Bo 4635 (#4)
    * BATIAI-824: Updating many variables to support dynamic config (#2)
    
    * BATIAI-824: Updating mysql with lots of variables to support rapidfort prod deploy
    
    * BATIAI-824: Readme docs
    
    * updated instance_class and tags
    
    * adding snapshot_identifier for disaster recovery
    
    * adding to .gitignore file
    
    ---------
    
    Co-authored-by: Charles Bushong <bushong1@gmail.com>

--- variables.tf
+++ variables.tf
@@ -5,6 +5,10 @@ variable "engine" {
 variable "engine_version" {
   default = "8.0.mysql_aurora.3.02.0"
 }
+variable "auto_minor_version_upgrade" {
+  type    = bool
+  default = false
+}
 variable "publicly_accessible" {
   default = "false"
 }
@@ -14,6 +18,10 @@ variable "vpc_id" {}
 variable "subnets" {
   type = list(string)
 }
+variable "instance_class" {
+  type    = string
+  default = "db.r5.xlarge"
+}
 
 variable "allowed_security_groups" {
   type = list(string)
@@ -22,6 +30,7 @@ variable "allowed_security_groups" {
 variable "master_username" {}
 variable "database_name" {}
 variable "tags" {
+  type    = map(string)
   default = {
     Owner = "Batcave"
   }
@@ -33,4 +42,35 @@ variable "route53_record_name" {}
 variable "worker_security_group_id" {}
 variable "cluster_security_group_id" {}
 variable "cluster_primary_security_group_id" {}
+variable "apply_immediately" {
+  type    = bool
+  default = false
+}
+variable "skip_final_snapshot" {
+  type    = bool
+  default = false
+}
 
+variable "enabled_cloudwatch_logs_exports" {
+  type    = list(any)
+  default = ["general"]
+}
+variable "create_db_subnet_group" {
+  type    = bool
+  default = true
+}
+variable "create_security_group" {
+  type    = bool
+  default = true
+}
+
+variable "iam_database_authentication_enabled" {
+  type    = bool
+  default = true
+}
+
+variable "snapshot_identifier" {
+  default = null
+  type = string
+  description = "If specified creates this database from a snapshot. Default is null.  Be warned that modifying this value on an already created database _WILL_ destroy/recreate the whole cluster."
+}
