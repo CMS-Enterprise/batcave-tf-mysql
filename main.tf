42acb380314bbbaf394733c271e4c282bbfaa717
GitLab CI <jloi@revacomm.com>
2023-11-14 07:01:02 -1000

    Test

--- main.tf
+++ main.tf
@@ -74,7 +74,15 @@ resource "aws_rds_cluster_parameter_group" "db_cluster_parameter_group" {
   }
 }
 
-resource "aws_route53_record" "www" {
+resource "aws_route53_record" "www-writer" {
+  zone_id = var.route53_zone_id
+  name    = var.route53_record_name
+  type    = "CNAME"
+  ttl     = "60"
+  records = ["${module.aurora.cluster_endpoint}"]
+}
+
+resource "aws_route53_record" "www-reader" {
   zone_id = var.route53_zone_id
   name    = var.route53_record_name
   type    = "CNAME"
