0c7e37c7b00b8dce961588fba3ff2a3ee8d631e0
Spencer Ho <sho@revacomm.com>
2024-07-09 11:45:36 -1000

    making variables null

--- variables.tf
+++ variables.tf
@@ -56,25 +56,25 @@ variable "tags" {
 
 variable "route53_zone_id" {
   type = string
-  default = ""
+  default = null
 }
 
 variable "route53_record_name" {
   type = string
-  default = ""
+  default = null
 }
 
 variable "worker_security_group_id" {
   type = string
-  default = ""
+  default = null
 }
 variable "cluster_security_group_id" {
   type = string
-  default = ""
+  default = null
 }
 variable "cluster_primary_security_group_id" {
   type = string
-  default = ""
+  default = null
 }
 variable "apply_immediately" {
   type    = bool
